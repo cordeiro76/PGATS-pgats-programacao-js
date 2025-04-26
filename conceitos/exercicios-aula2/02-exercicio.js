c9000730f13ae0ad8c5807352bcc3141bf7a007d
Jessica <cordeiro76@hotmail.com>
2025-04-26 12:03:28 -0300

    test

--- conceitos/exercicios-aula2/02-exercicio.js
+++ conceitos/exercicios-aula2/02-exercicio.js
@@ -1,9 +1,20 @@
 let nome = 'Kiara'
 let pesoKG = 38.9
-let estoqueAtualGramas = 10000
+let estoqueAtualGramas = 100000
 
 const racaoRecomendada = pesoKG * 30
 const diasEstoque = estoqueAtualGramas / racaoRecomendada
 
 
 console.log(`Nome: ${nome}\nPeso: ${pesoKG}\nQuantidade de ração recomendada por dia: ${racaoRecomendada} gramas\nQuantos dias o estoque atual vai durar: ${Math.floor(diasEstoque)} dias`)
+
+
+/**
+echo "# PGATS-pgats-programacao-js" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/cordeiro76/PGATS-pgats-programacao-js.git
+git push -u origin main
+ */
