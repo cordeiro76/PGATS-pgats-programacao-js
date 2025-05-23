e4d995ed9d4460d7e2cac93a4b4986730e50fc8f
Jessica <cordeiro76@hotmail.com>
2025-05-23 20:09:02 -0300

    melhorando function verificarSePodeSerAdotado

--- conceitos/trabalho/trabalho.js
+++ conceitos/trabalho/trabalho.js
@@ -4,18 +4,24 @@ function geradorDeTagsDeIdentificacao(nome) {
 
 function verificarSePodeSerAdotado(idade, porte) {
     if (idade == 1 && porte === 'M') {
-      //  console.log(`Pode ser Adotado!`)
+        console.log(`Pode ser Adotado pela idade e Porte!`)
+        return true
+    } else if (idade <= 1 && porte === 'P') {
+        console.log(`Pode ser Adotado pela idade!`)
+        return true
+    } else if (idade <= 1 && porte === 'G') {
+        console.log(`Pode ser Adotado pela idade!`)
         return true
     } else {
-       // console.log(`Não pode ser Adotado.`)
+        console.log(`Não pode ser Adotado.`)
         return false
     }
-
 }
+
 
 function calcularConsumoDeRacao(nome, estoque, peso) {
     const consumoDiario = peso * 300
-   // console.log(`Peso: ${peso}. Nome: ${nome}. Estoque: ${estoque}`)
+    console.log(`Peso: ${peso}. Nome: ${nome}. Estoque: ${estoque}`)
     return consumoDiario
 }
 
@@ -24,13 +30,15 @@ function decidirTipoDeAtividadePorPorte(porte) {
         pequeno: 'brincar dentro de casa',
         médio: 'caminhada no quarteirão',
         grande: 'correr no parque'
-    };
+    }
 
     return atividades[porte.toLowerCase()] || 'porte inválido';
+    
 }
 
-async function buscarDadoAsync(dog = 'Pipoca') {
+async function buscarDadoAsync(dog = 'pipoca') {
     return dog.charAt(0).toUpperCase() + dog.slice(1).toLowerCase();
+    
 }
 
 
@@ -41,3 +49,4 @@ export {
     decidirTipoDeAtividadePorPorte,
     buscarDadoAsync
 }
+
